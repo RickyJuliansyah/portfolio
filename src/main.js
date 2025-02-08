c1f89dabf1a522f65cda5342816fdc7ed95b41c5
RickyJuliansyah <rickyjuliansyah7@gmail.com>
2025-02-08 16:09:56 +0700

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-// import './script.js'
-import App from './App.vue'
-import router from './routes/index'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
--- src/page/NotFound.vue
+++
@@ -1,3 +0,0 @@
-<template>
-<p>dgksdbnksgk</p>
-</template>
